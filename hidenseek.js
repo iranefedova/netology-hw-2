6b05990d10fea183da33cbd7991330f5b40b387b
Ira Nefedova <iraonegina@yandex.ru>
2017-03-28 09:42:53 +0300

    modified seek function

--- hidenseek.js
+++ hidenseek.js
@@ -4,12 +4,15 @@ const PokemonList = require('./Pokemon').PokemonList;
 const FOLDER_COUNT = 10;
 const fs = require('fs');
 
+// сколько прятать
 const wat = (a, b) => a > b ? b : a;
 
+// вернуть целое случайное число в диапазоне (границы - включительно)
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// вернуть массив случайных уникальных чисел - номера папок, куда прятать покемонов
 function getUniqueRnd(min, max, size) {
   let tmp = new Array();
   for (let i=0; i<size; i++) {
@@ -22,6 +25,7 @@ function getUniqueRnd(min, max, size) {
   return tmp;
 }
 
+// формат имени папок
 const pad = (str, max) => {
     str = str.toString();
     return str.length < max ? pad("0" + str, max) : str;
@@ -36,7 +40,7 @@ function hide(path, pok_list) {
   hide_folders(path).then (response => {
       hide_pokemons(path, for_hide);
     })
-
+    console.log('Я спрятал');
     return for_hide;
 }
 
@@ -64,14 +68,38 @@ function hide_pokemons(path, for_hide) {
 }
 
 
-function seek(path) {
+function seek(path, callback) {
   let found = new PokemonList();
-  for (let i = 1; i <= FOLDER_COUNT; i++) {
-    fs.readdir(path + '/' + pad(i,2), (err, files) => {
-      if (err) throw err;
-      
-    })
-  }
+  let a = 0;
+    for (let i = 1; i <= FOLDER_COUNT; i++) {
+      seek_pokemons(path, i).then (response => {
+          if (response != null) {
+            found.push(response);
+          }
+          a++;
+          if (a == FOLDER_COUNT) callback(found);
+        })
+    }
+}
+
+function seek_pokemons(path, i) {
+  return new Promise(function(resolve, reject) {
+      fs.readdir(path + '/' + pad(i,2), (err, files) => {
+        if (err) throw err;
+        if (files.length > 0) {
+          files.forEach(file => {
+            fs.readFile(path + '/' + pad(i, 2) + '/' + file, 'utf8', function (err, data) {
+              if (err) reject(err);
+              let arr = data.split('|');
+              let p = new Pokemon(arr[0], arr[1]);
+              resolve(p);
+            });
+          });
+        } else {
+          resolve(null);
+        }
+      })
+  });
 }
 
 
--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PokemonList = require('./Pokemon').PokemonList;
 let lost = new PokemonList(new Pokemon("Charmander", 1), new Pokemon("Blastoise", 4), new Pokemon("Metapod", 3));
 
 console.log(hide('./field', lost));
-console.log('Я спрятал');
 
-let found = seek('./field');
-console.log('Я нашёл');
-console.log(found);
+let found = seek('./field', (res) => {
+  console.log('Я нашел');
+  console.log(res);
+});
